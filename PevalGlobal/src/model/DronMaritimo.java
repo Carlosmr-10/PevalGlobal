31e3693631a74b69955471e9870856490cc72ca5
user <user@DESKTOP-B366R1N>
2026-02-27 16:14:49 +0100

    Clase dron marítimo documentada

--- PevalGlobal/src/model/DronMaritimo.java
+++ PevalGlobal/src/model/DronMaritimo.java
@@ -1,10 +1,81 @@
 package model;
 
+/**
+ * Clase que representa un dron especializado en entornos marítimos.
+ * Esta clase hereda de la clase base Dron y añade la variable de resistencia a la salinidad.
+ * @author María Roche Durán
+ * @since 27/02/2026
+ * @version 1.0.0
+ */
 public class DronMaritimo extends Dron {
 
+	/**
+	 * Variable que define el factor de resistencia a la salinidad (0-100).
+	 */
+	private int resistenciaSal;
+
+	/**
+	 * Constructor con atributos específicos para el dron marítimo.
+	 * @param identificador Define el identificador del dron.
+	 * @param autonmíaMax Define la autonomía máxima del dron en minutos. 
+	 * @param velocidadMax Define la velocidad máxima del dron. 
+	 * @param operativo Define si el dron está activo o no. 
+	 * @param resistenciaSal Define el factor de resistencia a la salinidad (0-100).
+	 */
+	public DronMaritimo(String identificador, int autonmíaMax, float velocidadMax, boolean operativo, int resistenciaSal) {
+		super(identificador, autonmíaMax, velocidadMax, operativo);
+		this.resistenciaSal = resistenciaSal;
+	}
+
+	/**
+	 * Calcula el tiempo de respuesta para una misión marítima basándose en la distancia.
+	 * @param distancia La distancia de la misión.
+	 * @return El tiempo calculado en minutos.
+	 * @throws Exception Si el dron no está operativo, la distancia es negativa o excede la autonomía. 
+	 */
 	@Override
-	protected void calculoTiempoRespuesta() {
-		// TODO Auto-generated method stub
-		
+	public double calculoTiempoRespuesta(double distancia) throws Exception {
+		// Condición, si la distancia es negativa, lanzar una excepción 
+		if (distancia < 0) {
+			throw new Exception("La distancia no puede ser negativa.");
+		}
+
+		// Otra condición para saber el estado del dron (debe lanzar excepción si no está operativo) 
+		if (!this.operativo) {
+			throw new Exception("El dron no está operativo.");
+		}
+
+		// Dividir la distancia de la misión entre la velocidad máxima 
+		double tiempo = distancia / this.velocidadMax;
+
+		// Condición para saber y calcular si la resistencia a la salinidad es menor que 30, aumentar el tiempo en un 20% 
+		if (this.resistenciaSal < 30) {
+			tiempo = tiempo * 1.2;
+		}
+
+		// Comprobar que el tiempo calculado no exceda la autonomía disponible 
+		if (tiempo > this.autonmíaMax) {
+			throw new Exception("El tiempo calculado excede la autonomía disponible.");
+		}
+
+		return tiempo;
+	}
+
+	/**
+	 * Metodo que recoge el factor de resistencia a la salinidad.
+	 * @return una variable con la resistencia a la salinidad.
+	 */
+	public int getResistenciaSal() {
+		return resistenciaSal;
+	}
+
+	/**
+	 * Metodo que establece el factor de resistencia a la salinidad.
+	 * @param resistenciaSal Define la resistencia a la salinidad (0-100).
+	 */
+	public void setResistenciaSal(int resistenciaSal) {
+		if (resistenciaSal >= 0 && resistenciaSal <= 100) {
+			this.resistenciaSal = resistenciaSal;
+		}
 	}
 }
